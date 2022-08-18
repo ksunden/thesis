fd144f63121b71d234bed3c82ccdaaceddc03997
Kyle Sunden <git@ksunden.space>
2022-08-17 23:57:52 -0500

    Figure out minted stuff

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pdflatex --interaction=nonstopmode dissertation.tex
+pdflatex --interaction=nonstopmode -shell-escape dissertation.tex
 bibtex dissertation.aux
-pdflatex --interaction=nonstopmode dissertation.tex
-pdflatex --interaction=nonstopmode dissertation.tex
+pdflatex --interaction=nonstopmode -shell-escape dissertation.tex
+pdflatex --interaction=nonstopmode -shell-escape dissertation.tex
