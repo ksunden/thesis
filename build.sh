c9dcf7b6e8e17f76410631d5428eeb3dfd369bde
Kyle Sunden <git@ksunden.space>
2022-10-02 19:27:25 -0500

    Add yaq paper, wrightsim paper, fill out refs

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pdflatex --interaction=nonstopmode -shell-escape dissertation.tex
-bibtex dissertation.aux
+biber dissertation
 makeglossaries dissertation
 pdflatex --interaction=nonstopmode -shell-escape dissertation.tex
 pdflatex --interaction=nonstopmode -shell-escape dissertation.tex
